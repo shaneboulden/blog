686b355ff33103b41e92b7dca9b5e5d938590b23
shaneboulden <shane.boulden@gmail.com>
2023-09-07 15:14:29 +1000

    Latest GTFO P2 changes

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home({ posts }) {
                       <div className="text-base font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
-                          className="text-primary-800 hover:text-primary-900 dark:hover:text-primary-400"
+                          className="text-primary-800 dark:text-primary-700 hover:text-primary-900 dark:hover:text-primary-400"
                           aria-label={`Read "${title}"`}
                         >
                           Read more &rarr;
@@ -82,7 +82,7 @@ export default function Home({ posts }) {
         <div className="flex justify-end text-base font-medium leading-6">
           <Link
             href="/blog"
-            className="text-primary-800 hover:text-primary-900 dark:hover:text-primary-400"
+            className="text-primary-800 dark:text-primary-700 hover:text-primary-900 dark:hover:text-primary-400"
             aria-label="all posts"
           >
             All Posts &rarr;
