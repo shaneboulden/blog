904ddcd3356f18efafb5375036beddc2107a89ed
shaneboulden <shane.boulden@gmail.com>
2023-12-17 08:23:57 +1000

    Add screensaver draft

---
+++ components/IFrameEmbed.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const IFrameEmbed = ({ src }) => (
+  <div className="iframe-embedded">
+    <iframe
+      width="853"
+      height="480"
+      src={ src }
+      style={{ border: '0px', overflow: 'hidden' }}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded webpage"
+    />
+  </div>
+);
+
+IFrameEmbed.propTypes = {
+  src: PropTypes.string.isRequired
+};
+
+export default IFrameEmbed;
