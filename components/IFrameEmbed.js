13598ae05709b2e648dfb4bcedfa7e7f046cddae
shaneboulden <shane.boulden@gmail.com>
2023-12-17 08:31:18 +1000

    Resize iframe

--- components/IFrameEmbed.js
+++ components/IFrameEmbed.js
@@ -5,6 +5,7 @@ const IFrameEmbed = ({ src }) => (
   <div className="iframe-embedded">
     <iframe
       height="480"
+      width="100%"
       src={ src }
       style={{ border: '0px', overflow: 'hidden' }}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
